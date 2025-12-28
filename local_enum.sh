f63d39315447f8e0b2c53e1fb7d793c425c8f5c0
Kedr-0 <Rn_A3_50f14@hotmail.com>
2025-12-28 00:39:02 -0500

    Create local_enum.sh

---
+++ local_enum.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+clear
+
+# Colores
+CYAN='\033[1;36m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+RED='\033[1;31m'
+RESET='\033[0m'
+
+echo -e "${CYAN}"
+echo "────────────────────────────────────────────"
+echo "            LOCAL ENUM"
+echo "     Local Linux Enumeration Tool"
+echo "────────────────────────────────────────────"
+echo -e "${RESET}"
+
+echo -e "${YELLOW}  Author     : Kedr-0${RESET}"
+echo -e "${YELLOW}  Scope      : Local Enumeration${RESET}"
+echo -e "${YELLOW}  Start Time : $(date)${RESET}"
+
+echo -e "${WHITE}────────────────────────────────────────────────────────────────────────────${RESET}"
+
+# Información del sistema
+echo -e "${CYAN}[+] Target Information${RESET}"
+echo -e "    ├─ Hostname : $(hostname)"
+echo -e "    ├─ User     : $(whoami)"
+echo -e "    ├─ UID      : $(id -u)"
+echo -e "    ├─ Kernel   : $(uname -r)"
+echo -e "    └─ OS       : $(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '\"')"
+
+echo
+echo -e "${GREEN}[+] Initializing local enumeration modules...${RESET}"
+sleep 1
+
+echo -e "${RED}\nUsuarios Activos\n${RESET}"
+
+grep -e '/bin/sh' -e '/bin/bash' -e '/bin/zsh' /etc/passwd | awk -F ':' '{print $1}'  | sed -n "$i"p
+
+echo -e "${RED}\nBinarios SUID\n${RESET}"
+
+find / -perm -4000 2>/dev/null
+
+echo -e "${RED}\nCapabilities\n${RESET}"
+
+getcap -r / 2>/dev/null
+
+echo -e "${RED}\nTareas Cron\n${RESET}"
+
+ls -la /etc/cron*
+echo -e "\n"
+tail -n6 /etc/crontab
